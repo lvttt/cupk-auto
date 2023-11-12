e6b3be6dbe387951635df3f11aba761d64f1f92e
lvt <2227152540@qq.com>
2023-11-12 16:45:03 +0800

    update

--- serve/blueprints/jwxt/__init__.py
+++ serve/blueprints/jwxt/__init__.py
@@ -1,19 +1,64 @@
-from flask import Blueprint
-
+from flask import Blueprint, request
+from Response import Success, Error
 
 jwxt_bp = Blueprint('jwxt', __name__)
 
+
 @jwxt_bp.route('/login')
 def Login():
+    """
+    登录教务系统
+    """
     from . import login
     return login.login()
 
+
 @jwxt_bp.route('/xk/login')
 def LoginXK():
+    """
+    登录选课系统
+    """
     from . import loginXK
     return loginXK.loginXK()
 
+
 @jwxt_bp.route('/xk/getReadyScore')
 def GetReadyScore():
+    """
+    获取已选课的学分
+    """
     from . import selectCourse
     return selectCourse.getReadyScore()
+
+
+@jwxt_bp.route('/xk/queryCourseList', methods=['POST'])
+def QueryCourseList():
+    """
+    查询课程列表: 本学期计划选课
+    """
+    try:
+        request_data = request.get_json()
+        mode = request_data['mode'] # 选课模式
+        params = request_data['params'] # 查询参数
+    except:
+        return Error(msg="参数错误").toJson()
+    from . import selectCourse
+    return selectCourse.queryCourseList(params, mode)
+
+
+@jwxt_bp.route('/xk/selectCourse', methods=['POST'])
+def SelectCourse():
+    """
+    选课: 本学期计划选课
+    """
+    # get flask request.args then convert to json and get courselist
+    try:
+        request_data = request.get_json()
+        courselist = request_data['courselist'] # 课程id列表
+        mode = request_data['mode'] # 选课模式
+    except:
+        return Error(msg="参数错误").toJson()
+    from . import selectCourse
+    return selectCourse.selectCourse(courselist, mode)
+
+
--- serve/blueprints/jwxt/selectCourse.py
+++ serve/blueprints/jwxt/selectCourse.py
@@ -16,3 +16,69 @@ def getReadyScore():
     except Exception as e:
         print(e)
         return Error(msg="出错了").toJson()
+
+def queryCourseList(params,mode=0):
+    # params = {
+    #     'kcxx': '',
+    #     'skls': '',
+    #     'skxq': '',
+    #     'skjc': '',
+    #     'sfym': 'false',
+    #     'sfct': 'false',
+    #     'sfxx': 'false',
+    # }
+    data = {
+        'sEcho': '1',
+        'iColumns': '13',
+        'sColumns': '',
+        'iDisplayStart': '0',
+        'iDisplayLength': '999',
+        'mDataProp_0': 'kch',
+        'mDataProp_1': 'kckxh',
+        'mDataProp_2': 'kcmc',
+        'mDataProp_3': 'xf',
+        'mDataProp_4': 'skls',
+        'mDataProp_5': 'sksj',
+        'mDataProp_6': 'skdd',
+        'mDataProp_7': 'ktmc',
+        'mDataProp_8': 'xkrs',
+        'mDataProp_9': 'syrs',
+        'mDataProp_10': 'ctsm',
+        'mDataProp_11': 'xkyq',
+        'mDataProp_12': 'czOper',
+    }
+    url = app.config["QUERY_COURSE_URL"][mode]
+
+    if mode == 3:
+        # 公选课选课
+        data["mDataProp_7"] = "xkrs"
+        data["mDataProp_8"] = "syrs"
+        data["mDataProp_9"] = "ctsm"
+        data["mDataProp_10"] = "szkcflmc"
+
+    try:
+        response = session.post(url, params=params, data=data,
+             verify=False)
+        return Success(data=response.json()).toJson()
+    except Exception as e:
+        print(e)
+        return Error(msg="出错了").toJson()
+
+def selectCourse(courselist, mode=0):
+    try:
+        res = []
+        url = app.config["SELECT_COURSE_URL"][mode]
+        for courseId in courselist:
+            params = {
+                'jx0404id': courseId,
+                'xkzy': '',
+                'trjf': '',
+            }
+            response = session.get(url, params=params,
+                        verify=False)
+            # get respone.success
+            res.append(response.json())
+        return Success(data=res).toJson()
+    except Exception as e:
+        print(e)
+        return Error(msg="出错了").toJson()
